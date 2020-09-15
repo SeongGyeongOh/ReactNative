86f6260e0e0f86eec465bdeae692ab07a3c0f3c9
SeongGyeong <deborahoh94@gmail.com>
2020-09-15 14:42:30 +0900

    commit -react native

--- Ex04Layout/MainComponent.js
+++ Ex04Layout/MainComponent.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 import {Button, StyleSheet, View} from 'react-native';
 
+// position 속성의 웹과의 차이점 : 기본적으로 Comopnent-View의 속성이 relative로 들어가 있다.(기본 flex 속성)
+// 그래서 absolute를 줬을 때 해당 Component를 감싸는 부모 요소의 좌상단을 기준으로 움직인다!!!
+
 export default class MainComponent extends Component{
     render(){
         return(
-            <View style={{flex:1, flexDirection:'row', justifyContent:"space-evenly", alignItems:'center'}}>
-                <View style={{width:50, height:50, backgroundColor:'green'}}></View>
-                <View style={{width:50, height:50, backgroundColor:'red'}}></View>
-                <View style={{width:50, height:50, backgroundColor:'blue'}}></View>
+            <View style={ { flex:1, flexDirection:'column' }}>
+                {/* 크게 세로 1:2 */}
+                <View style={{flex:1, flexDirection:'row', backgroundColor:'green'}}>
+                    {/* 좌우 1:2 */}
+                    <View style={{flex:1, backgroundColor:'red'}}>
+                        <View style={{position:'absolute', backgroundColor:'white', width:50, height:50, top:10, left:10, zIndex:1}}></View>
+                        <View style={{position:'absolute', backgroundColor:'black', width:50, height:50, top:20, left:20}}></View>
+                    </View>
+                    <View style={{flex:2, backgroundColor:'black'}}>
+                    </View>
+                </View>
+                <View style={{flex:2, backgroundColor:'blue', flexDirection:'row'}}>
+                    {/* 좌우 1:2 */}
+                    <View style={{flex:2, backgroundColor:'yellow'}}>
+                        <View style={{position:'absolute', backgroundColor:'grey', width:50, height:50, top:50, left:100}}></View>
+                        <View style={{position:'absolute', backgroundColor:'pink', width:50, height:50, top:70, left:70}}></View>
+                    </View>
+                    <View style={{flex:1, backgroundColor:'orange'}}></View>
+                </View>
             </View>
         );
     }
 }
-
-// const styles=StyleSheet.create({
-//     root:{
-//         padding:16,
-//         flex:1,
-//     }
-// });
